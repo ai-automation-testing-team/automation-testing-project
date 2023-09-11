56940159f3ede355bb5a45762040456d7a0ffa39
runner <runner@fv-az395-562.f04nadzq13vercrdnegy2lfzkh.gx.internal.cloudapp.net>
2023-09-11 21:20:55 +0000

    AI suggested changes

--- src/test/java/org/endava/automation/testing/tests/TestNGPassedExamplesTest.java
+++ src/test/java/org/endava/automation/testing/tests/TestNGPassedExamplesTest.java
@@ -20,7 +20,7 @@ public class TestNGPassedExamplesTest extends BaseTest {
 	}
 	// Passed Scenario
 	@Test()
-	@DescAI
+	@DescAI(value = false, content = "[Test Name] - verifyTransferFundsFromSavingsToCheckingAccount\n[Test Summary] - This test verifies the successful transfer of funds from the Savings account to the Checking account.\n\n[Test Step] - User navigates to the Account Activity page and selects the Savings account - [Test Data] - N/A - [Test Result] - The Savings account is selected successfully.\n[Test Step] - User calculates the sum of deposits and verifies that it matches the expected value - [Test Data] - N/A - [Test Result] - The sum of deposits is correct.\n[Test Step] - User calculates the sum of withdrawals and verifies that it matches the expected value - [Test Data] - N/A - [Test Result] - The sum of withdrawals is correct.\n[Test Step] - User makes a money transfer from the Savings account to the Checking account with the specified amount and description - [Test Data] - Amount: $1934.3, Description: Test Description - [Test Result] - The transfer is successfully submitted.\n[Test Step] - User validates that the transaction amount matches the expected amount - [Test Data] - N/A - [Test Result] - The transaction amount is correct.")
 	public void verifyTransferFundsFromSavingsToCheckingAccount() {
 		handleAllureDescription();
 
