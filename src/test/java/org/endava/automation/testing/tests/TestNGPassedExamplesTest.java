08872e9725f0044d4295e9956f91a68d0be1067d
vsushelski <Viktor.Sushelski@endava.com>
2023-09-11 15:22:31 +0200

    AI suggested changes

--- src/test/java/org/endava/automation/testing/tests/TestNGPassedExamplesTest.java
+++ src/test/java/org/endava/automation/testing/tests/TestNGPassedExamplesTest.java
@@ -20,7 +20,7 @@ public class TestNGPassedExamplesTest extends BaseTest {
 	}
 	// Passed Scenario
 	@Test()
-	@DescAI(value = false, content = "[Test Name] - verifyTransferFundsFromSavingsToCheckingAccount\n[Test Summary] - This test verifies that funds can be transferred successfully from the Savings account to the Checking account.\n\n[Test Step] - User navigates to Account Activity to choose Account: Savings - [Test Data] - None - [Test Result] - The user successfully navigates to the Account Activity page and selects the Savings account.\n[Test Step] - User reads Deposits and calculate Deposits sum - [Test Data] - None - [Test Result] - The user reads the deposits and calculates the sum of all deposits.\n[Test Step] - User reads Withdrawals and calculate Withdrawals sum - [Test Data] - None - [Test Result] - The user reads the withdrawals and calculates the sum of all withdrawals.\n[Test Step] - User makes Money Transfer from Account: Savings to Account: Checking with Amount: 1934.3 and Description: Test Description - [Test Data] - None - [Test Result] - The user successfully initiates a money transfer from the Savings account to the Checking account with the specified amount and description.\n[Test Step] - User validates that Transaction is Submitted and returning the Transaction Amount - [Test Data] - None - [Test Result] - The user verifies that the transaction is successfully submitted and retrieves the transaction amount.\n[Test Step] - User validates that Transaction Amount is: $ 1934.3 - [Test Data] - None - [Test Result] - The user verifies that the transaction amount is correct and matches the expected value.")
+	@DescAI(value = false, content = "")
 	public void verifyTransferFundsFromSavingsToCheckingAccount() {
 		handleAllureDescription();
 
