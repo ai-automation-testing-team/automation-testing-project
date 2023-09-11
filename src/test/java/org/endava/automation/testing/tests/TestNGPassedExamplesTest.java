09be9a558e2da30892178550a94c80d7805b80e3
runner <runner@fv-az955-788.mlkcatuscfmejm4ctfapoghrmg.cx.internal.cloudapp.net>
2023-09-11 20:25:09 +0000

    AI suggested changes

--- src/test/java/org/endava/automation/testing/tests/TestNGPassedExamplesTest.java
+++ src/test/java/org/endava/automation/testing/tests/TestNGPassedExamplesTest.java
@@ -20,7 +20,7 @@ public class TestNGPassedExamplesTest extends BaseTest {
 	}
 	// Passed Scenario
 	@Test()
-	@DescAI
+	@DescAI(value = false, content = "[Test Name] - Verify Transfer Funds From Savings To Checking Account\n\n[Test Summary] - This test case aims to verify that funds can be successfully transferred from a savings account to a checking account.\n\n[Test Step] - User navigates to Account Activity to choose Account: Savings - [Test Data] - N/A - [Test Result] - The user successfully navigates to the Account Activity page and selects the Savings account.\n\n[Test Step] - User reads Deposits and calculate Deposits sum - [Test Data] - N/A - [Test Result] - The user successfully reads the deposits from the account and calculates the sum.\n\n[Test Step] - User reads Withdrawals and calculate Withdrawals sum - [Test Data] - N/A - [Test Result] - The user successfully reads the withdrawals from the account and calculates the sum.\n\n[Test Step] - User makes Money Transfer from Account: Savings to Account: Checking with Amount: 1934.3 and Description: Test Description - [Test Data] - Amount: 1934.3, Description: Test Description - [Test Result] - The user successfully initiates a money transfer from the Savings account to the Checking account with the specified amount and description.\n\n[Test Step] - User validates that Transaction is Submitted and returning the Transaction Amount - [Test Data] - N/A - [Test Result] - The user successfully validates that the transaction is submitted and retrieves the transaction amount.")
 	public void verifyTransferFundsFromSavingsToCheckingAccount() {
 		handleAllureDescription();
 
